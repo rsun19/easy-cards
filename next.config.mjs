60984a3afa97c467249e87a9a2333b97e0010c4c
rsun19 <robertssun1234@gmail.com>
2024-09-25 21:07:46 -0400

    simplified variable changing

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,22 @@
 https://medium.com/@rohitkumarkhatri/next-auth-in-app-router-of-next-js-7df037f7a2ad
 */
 
+const env = process.env.NODE_ENV;
+
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
-  env: {
-    // change between production and 
+  env: env === 'production' ? 
+  {
+    NEXT_PUBLIC_API_URL: "http://backend:9000",
+    NEXT_PUBLIC_CLIENT_TO_API_URL: "https://easyflashcards.org/clientAPI",
+    NEXT_PUBLIC_REDIRECT_BASE: "https://easyflashcards.org"
+  }
+  : {
     NEXT_PUBLIC_API_URL: "http://localhost:9000",
     NEXT_PUBLIC_CLIENT_TO_API_URL: "http://localhost:9000",
-    // NEXT_PUBLIC_API_URL: "http://backend:9000",
-    // NEXT_PUBLIC_CLIENT_TO_API_URL: "https://easyflashcards.org/clientAPI"
-  },
+    NEXT_PUBLIC_REDIRECT_BASE: "/"
+  }
 };
 
 export default nextConfig;
